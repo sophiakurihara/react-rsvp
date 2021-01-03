e615ad1b3a5668a5391151d1f145c6df53f969d7
sophiakurihara <sophia.sudiacal@gmail.com>
2021-01-03 14:18:29 -0600

    added guest confirmation handler and separated Guest component

--- src/App.js
+++ src/App.js
@@ -21,6 +21,19 @@ class App extends Component {
         ]
     };
 
+    toggleConfirmationAt = indexToChange =>
+        this.setState({
+            guests: this.state.guests.map((guest,index) => {
+                if (index === indexToChange){
+                    return {
+                        ...guest,
+                        isConfirmed: !guest.isConfirmed
+                    }
+                }
+                return guest;
+            })
+        });
+
     getTotalInvited = () => this.state.guests.length;
     getAttendingGuests = () => {
        let totalAttending = 0;
@@ -65,7 +78,10 @@ class App extends Component {
                         </tr>
                         </tbody>
                     </table>
-                    <GuestList guests={this.state.guests}/>
+                    <GuestList
+                        guests={this.state.guests}
+                        toggleConfirmationAt={this.toggleConfirmationAt}
+                    />
                 </div>
             </div>
         );
---
+++ src/Guest.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Guest = (props) => {
+    return (
+        <li>
+            <span>{props.name}</span>
+            <label>
+                <input
+                    type="checkbox"
+                    checked={props.isConfirmed}
+                    onChange={props.handleConfirmation}
+                /> Confirmed
+            </label>
+            <button>edit</button>
+            <button>remove</button>
+        </li>
+    );
+};
+
+Guest.propTypes = {
+    name: PropTypes.string.isRequired,
+    isConfirmed: PropTypes.bool.isRequired,
+    handleConfirmation: PropTypes.func.isRequired
+};
+
+export default Guest;
